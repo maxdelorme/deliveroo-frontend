bf4a6e97dd71b55c59b45d9e6fa2b670c7a0df10
Maxime Delorme <maxime@familledelorme.com>
2025-11-18 20:31:03 +0100

    Manage basket for small device
    display number of items and price and is collapsable

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,24 +1,54 @@
 import { useBasketContext } from "./BasketContentProvider";
 import BasketItem from "./BasketItem";
 import myFormat from "../utils/EuroFormat";
-
+import { useState } from "react";
 const Basket = () => {
   const { basketContent } = useBasketContext();
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <div className="basket">
-      <div className="bottomMsg ">
-        <p>Voir le panier</p>
-      </div>
-      <div className="panel">
-        {!basketContent || !basketContent.length ? (
-          <>
+      {!basketContent || !basketContent.length ? (
+        <>
+          <div className="panel empty">
             <div className="validate">Valider mon panier</div>
-            <div className="content">Votre panier est vide</div>
-          </>
-        ) : (
-          <>
-            <div className="validate withitems">Valider mon panier</div>
+            <div className="content">
+              <p>Votre panier est vide</p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          {!isExpanded && (
+            <p
+              onClick={() => {
+                setIsExpanded(true);
+              }}
+              className="mini-basket"
+            >
+              <span className="nbItems">{basketContent.length}</span>
+              <span>Voir le panier</span>
+              <span>
+                {myFormat(
+                  basketContent.reduce((sum, item) => {
+                    sum += item.count * item.price;
+                    return sum;
+                  }, 2.5)
+                )}
+              </span>
+            </p>
+          )}
+
+          <div className={"panel withitems " + (isExpanded ? " expanded" : "")}>
+            <div
+              className="CloseMe"
+              onClick={() => {
+                setIsExpanded(false);
+              }}
+            >
+              X
+            </div>
+            <div className="validate ">Valider mon panier</div>
             <div className="listItems">
               {basketContent.map((item) => (
                 <BasketItem key={item.id + "-" + item.count} item={item} />
@@ -54,9 +84,9 @@ const Basket = () => {
                 )}
               </span>
             </div>
-          </>
-        )}
-      </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
